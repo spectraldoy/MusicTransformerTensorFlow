170f217e5bd4bee9b6e11157757ae315f6391d3e
spectraldoy <littleguyisfum@gmail.com>
2021-03-20 16:24:49 +0530

    updated copyright

--- music_transformer.py
+++ music_transformer.py
@@ -1,16 +1,3 @@
-"""
-Copyright 2020 Aditya Gomatam.
-
-This file is part of Music-Transformer (https://github.com/spectraldoy/Music-Transformer), my project to build and
-train a Music Transformer. Music-Transformer is open-source software licensed under the terms of the GNU General
-Public License v3.0. Music-Transformer is free software: you can redistribute it and/or modify it under the terms of
-the GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
-or (at your option) any later version. Music-Transformer is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details. A copy of this license can be found within the GitHub repository
-for Music-Transformer, or at https://www.gnu.org/licenses/gpl-3.0.html.
-"""
-
 import tensorflow as tf
 import numpy as np
 
